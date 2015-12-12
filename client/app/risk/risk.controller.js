9c72d43af000174ee07274c3bace7dc0854a9010
prajaktawani <prajaktamwani@gmail.com>
2015-12-12 22:43:19 +0530

    added industry to add Strategy page

--- client/app/risk/risk.controller.js
+++ client/app/risk/risk.controller.js
@@ -11,3 +11,13 @@ angular.module('atpexpApp')
 	      $scope.objects = strategies;
   })
   })
+
+  .controller('indusCtrl', function ($scope, $http) {
+
+   
+	    $http.get('/api/industry').success(function (industries) {
+	    
+	      console.log(industries);
+	      $scope.objects = industries;
+  })
+  })

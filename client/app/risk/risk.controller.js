efd7521e059198711c02b600aae0c638b3f8d07e
prajaktawani <prajaktamwani@gmail.com>
2015-12-12 22:03:44 +0530

    display strategy

--- client/app/risk/risk.controller.js
+++ client/app/risk/risk.controller.js
@@ -2,7 +2,12 @@
 
 angular.module('atpexpApp')
 
-  .controller('riskCtrl', function ($scope, $modal) {
+  .controller('riskCtrl', function ($scope, $http) {
 
-    
+   
+	    $http.get('/api/strategy').success(function (strategies) {
+	    
+	      console.log(strategies);
+	      $scope.objects = strategies;
+  })
   })

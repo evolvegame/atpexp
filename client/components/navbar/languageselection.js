ab63067fe70f5d164d97a74b8ffad8fb2871749d
subbu16 <subbu16@gmail.com>
2015-12-29 11:21:22 +0100

    Translate
    Actual Translate functionality

---
+++ client/components/navbar/languageselection.js
@@ -0,0 +1,28 @@
+angular.module('atpexpApp') .directive('ngTranslateLanguageSelect', function (localeservice) { 'use strict';
+
+        return {
+            restrict: 'A',
+            replace: true,
+            template: ''+
+            '<div class="language-select" ng-if="visible">'+
+                '<label>'+
+                    '{{"directives.language-select.Language" | translate}}:'+
+                    '<select ng-model="currentLocaleDisplayName"'+
+                        'ng-options="localesDisplayName for localesDisplayName in localesDisplayNames"'+
+                        'ng-change="changeLanguage(currentLocaleDisplayName)">'+
+                    '</select>'+
+                '</label>'+
+            '</div>'+
+            '',
+            controller: function ($scope) {
+                $scope.currentLocaleDisplayName = localeservice.getLocaleDisplayName();
+                $scope.localesDisplayNames = localeservice.getLocalesDisplayNames();
+                $scope.visible = $scope.localesDisplayNames &&
+                $scope.localesDisplayNames.length > 1;
+
+                $scope.changeLanguage = function (locale) {
+                    localeservice.setLocaleByDisplayName(locale);
+                };
+            }
+        };
+    });

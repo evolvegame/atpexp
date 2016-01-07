88973138a17e03e5e49438b96807f1e56edea152
Rangarajan Ramadass <Rangarajan_Ramadass@ad.infosys.com>
2016-01-07 16:31:59 +0530

    Changes related to changing roundLevelInformation of team document to an array and fixed the refresh issue in company screen.

--- client/app/company/company.controller.js
+++ client/app/company/company.controller.js
@@ -4,35 +4,45 @@ angular.module('atpexpApp')
 
   .controller('CompanyCtrl', function ($scope, $http, Auth,Team, $rootScope, Project) {
     
-    $http.get('/api/projects').success(function (projects) {
-//      console.log(projects)
-      $scope.getCurrentTeam = Auth.getCurrentTeam;
-      
-      for (var i = 0; i<projects.length;i++) {
-         var obj = projects[i];
-         for(var j = 0; j < $scope.getCurrentTeam().roundLevelInformation.project.length; j++) {
-        	 var proj = $scope.getCurrentTeam().roundLevelInformation.project[j];
-        	 if (proj == obj._id) {
-        		 obj.switchStatus = true;
-//        		 console.log("project id - " + obj._id);
-        		 break;
-        	 }   	 
-         }
-      }
-      $scope.objects = projects;
-      $scope.totalItems = $scope.objects.length;
-      $scope.currentPage = 1;
-      $scope.numPerPage = 5;      
+	  $http.get('/api/rounds/currentRound').success(function(round){  
+    	  $scope.currentRound = round.round;
+      });
+	  
+	  $http.get('/api/team/me').success(function (team) {     
+			$rootScope.team = team;     
+			$rootScope.roundLevelInformation = $rootScope.team.roundLevelInformation;
+	  });
+	  
+	  $http.get('/api/projects').success(function (projects) {
+		  $scope.referentialListOfProjects = projects;
+	      
+	      var currentRoundLevelInformationIndex = $scope.currentRound - 1;
+	      
+	      for (var i = 0; i<projects.length;i++) {
+	         var obj = projects[i];
+	         for(var j = 0; j < $rootScope.team.roundLevelInformation[currentRoundLevelInformationIndex].project.length; j++) {
+	        	 var proj = $rootScope.team.roundLevelInformation[currentRoundLevelInformationIndex].project[j];
+	        	 if (proj == obj._id) {
+	        		 obj.switchStatus = true;
+	        		 break;
+	        	 }   	 
+	         }
+	      }
+	      
+	      $rootScope.objects = projects;
+	      $scope.totalItems = $scope.objects.length;
+	      $scope.currentPage = 1;
+	      $scope.numPerPage = 5;    
     });
     
     $http.get('/api/departments').success(function(departments){
-//    	console.log('Department - ' + departments)
+    	$scope.referentialListOfDepartments = departments;
     	for (var i = 0; i < departments.length ; i++) {
     		var obj = departments[i];
     		var departmentFound = false;
-    		
-    			for (var j = 0; j < $scope.getCurrentTeam().roundLevelInformation.department.length; j++) {
-        			var depart = $scope.getCurrentTeam().roundLevelInformation.department[j];
+    		var currentRoundLevelInformationIndex = $scope.currentRound - 1;
+    			for (var j = 0; j < $rootScope.team.roundLevelInformation[currentRoundLevelInformationIndex].department.length; j++) {
+        			var depart = $rootScope.team.roundLevelInformation[currentRoundLevelInformationIndex].department[j];
         			
         			if (depart.name == obj.name) {
         				departmentFound = true;
@@ -42,9 +52,6 @@ angular.module('atpexpApp')
         					if (sizeUnit.unit == depart.sizeUnit) {
         						sizeUnit.switchStatus = true;
         						obj.selectedCost = sizeUnit.cost;
-        					/*	console.log("Depart Name --> " + depart.name);
-        						console.log("SizeUnit --> " + sizeUnit.unit);
-        						console.log("SizeUnit --> " + sizeUnit.switchStatus);*/
         						break;		
         					}
         					
@@ -60,9 +67,6 @@ angular.module('atpexpApp')
     					if (sizeUnit.unit == 'Small') {
     						sizeUnit.switchStatus = true;
     						obj.selectedCost = sizeUnit.cost;
-    					/*	console.log("Depart Name --> " + depart.name);
-    						console.log("SizeUnit --> " + sizeUnit.unit);
-    						console.log("SizeUnit --> " + sizeUnit.switchStatus);*/
     						break;		
     					}
     					
@@ -72,54 +76,73 @@ angular.module('atpexpApp')
     		
     	}
     	
-    	
-    	$scope.departments = departments;
+    	$rootScope.departments = departments;
     });
     
-    $scope.bgcolorEnter = function(project) {
-    	project.background = "#FACC2E";
-    };
-
-    $scope.bgcolorLeave = function(project) {
-    	console.log($scope.projectClicked);
-    	if(project.lockedFromFurtherClicks) {
-    		return;
-    	}
-    	if(!$scope.projectClicked) {
-    		project.background = "#BDBDBD";
-    	} else {
-    		project.background = "#FACC2E";
-    	}
-    	$scope.projectClicked = false;
-    };
-    
     $scope.addProject= function(project) {
-//    	console.log('event ' + event);
-    	console.log('state ' + project.switchStatus);
-//    	console.log('Selected project name -- ' + dom.name);
-    	console.log("Hi i am here " + project.name);
-    	console.log("Hi i am here " + project.amount);
-    	Project.teamCompany(project);
-        refresh();
+    	var currentRoundLevelInformationIndex = $scope.currentRound - 1;
+    	Project.teamCompany(project).$promise.then(function(team){
+    		var projects = $scope.referentialListOfProjects;
+    		for (var i = 0; i<projects.length;i++) {
+    	         var obj = projects[i];
+    	         for(var j = 0; j < team.roundLevelInformation[currentRoundLevelInformationIndex].project.length; j++) {
+    	        	 var proj = team.roundLevelInformation[currentRoundLevelInformationIndex].project[j];
+    	        	 if (proj == obj._id) {
+    	        		 obj.switchStatus = true;
+    	        		 break;
+    	        	 }   	 
+    	         }
+    	      }
+    	      
+    		$rootScope.objects = projects;
+    	});
     };
     
     $scope.addDepartment = function(department, size) {
-    	console.log('department ' + department.name);
-    	console.log('size ' + JSON.stringify(size));
-//    	console.log('Selected project name -- ' + dom.name);
-//    	console.log("Hi i am here " + project.name);
-//    	console.log("Hi i am here " + project.amount);
-    	Team.teamDepartment(department, size);
-    	refresh();
+    	Team.teamDepartment(department, size).$promise.then(function(team){
+    		$rootScope.team = team;
+    		var departments = $scope.referentialListOfDepartments
+    		for (var i = 0; i < departments.length ; i++) {
+	    		var obj = departments[i];
+	    		var departmentFound = false;
+	    		var currentRoundLevelInformationIndex = $scope.currentRound - 1;
+	    			for (var j = 0; j < $rootScope.team.roundLevelInformation[currentRoundLevelInformationIndex].department.length; j++) {
+	        			var depart = $rootScope.team.roundLevelInformation[currentRoundLevelInformationIndex].department[j];
+	        			
+	        			if (depart.name == obj.name) {
+	        				departmentFound = true;
+	        				for (var k=0; k < obj.size.length; k++) {
+	        					var sizeUnit = obj.size[k];
+	        					
+	        					if (sizeUnit.unit == depart.sizeUnit) {
+	        						sizeUnit.switchStatus = true;
+	        						obj.selectedCost = sizeUnit.cost;
+	        					} else {
+	        						sizeUnit.switchStatus = false;
+	        					}
+	        					
+	        				}
+	        				
+	        			}
+	        		}
+	    			
+	    			if (!departmentFound) {
+	    				for (var k=0; k < obj.size.length; k++) {
+	    					var sizeUnit = obj.size[k];
+	    					
+	    					if (sizeUnit.unit == 'Small') {
+	    						sizeUnit.switchStatus = true;
+	    						obj.selectedCost = sizeUnit.cost;
+	    						break;		
+	    					}
+	    					
+	    				}
+	    			}    		
+    	}
+    	
+    	console.log(JSON.stringify(departments));
+    	$rootScope.departments = departments;
+    	});
     };
-    
-    //temporary Solution to refresh model object
-    function reloadPage(){ $window.location.reload(); }
-
-    function refresh(){
-        $http.get('/api/team/me').success( function (team){
-        $rootScope.team = team;
-        });
-    }
-    
+       
   })

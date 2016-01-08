541f4cdc25014c0a85d9e6c9575685ed3e27d10d
subbu16 <subbu16@gmail.com>
2016-01-08 09:33:41 +0100

    round service

--- client/components/services/round.service.js
+++ client/components/services/round.service.js
@@ -1,7 +1,26 @@
 'use strict';
 
 angular.module('atpexpApp')
-  .service('Round', function ($resource) {
-    this.round = $resource('/api/rounds');    
-  });
-  
+  .factory('Round', function ($resource) {
+      return $resource('/api/rounds/:roundId/:controller', {}, {
+          save: { method: 'POST' },
+          update: { method: 'PUT', params: {entryId: '@roundId'} },
+          currRound: {method:'GET', params: {controller:'currentRound'}}
+        }
+      );
+    });
+
+// angular.module('atpexpApp')
+//   .factory('Round', function ($resource) {
+//     return $resource('/api/rounds/:id/:controller', {
+//       id: '@_id'
+//     },
+//     {
+//       get: {
+//         method: 'GET',
+//         params: {
+//           controller:'currentRound'
+//         }
+//       }
+// 	  });
+//   });
